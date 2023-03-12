bca88c7b2865f2ef67a0a9c3c8e203fedeeff2da
wsbousman <85909621+wsbousman@users.noreply.github.com>
2023-03-11 19:31:15 -0600

    "Better" Component
    "What could you have done better?" input form component added.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Header from './components/header/index.js';
 import Drill from './components/drill/index.js';
 import Roll from './components/roll/index.js';
 import Well from './components/well/index.js';
+import Better from './components/better/index.js';
 import Addroll from './components/addroll/index.js';
 import Submit from './components/submit/index.js';
 
@@ -26,6 +27,7 @@ function App() {
       <Roll />
       <Addroll />
       <Well />
+      <Better />
       <Submit />
       </Router>
     </div>
---
+++ src/components/better/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function better() {
+
+  return (
+    <section>
+        <div className="label">
+            <p>what could you have done better?</p>
+        </div>
+        <div className="wrapper">
+            <form>
+                <input type="text" className="inputData" />     
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default better
